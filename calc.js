d972153f47f2d9651e9f827bd95b6090bcecbaf1
Landon Taylor <landontaylor@Landons-MacBook-Pro.local>
2022-01-18 14:17:54 -0700

    All math implementations are working

--- calc.js
+++ calc.js
@@ -14,8 +14,31 @@ reader.question("What would you like to calculate?", function(input){
 	num1 = Number(tokens[1]);
 	num2 = Number(tokens[2]);
 
+	if (mathSymbol === "+"){
+		console.log(num1 + num2);
+	}
+
+	if (mathSymbol === "-"){
+		console.log(num1 - num2);
+	}
+
+	if (mathSymbol === "/"){
+		console.log(num1 / num2);
+	}
+
+	if (mathSymbol === "*"){
+		console.log(num1 * num2);
+	}
+	if (mathSymbol === "sqrt"){
+		console.log(Math.sqrt(num1))
+	}
+
+	// console.log('mathSymmbol' , mathSymbol)
+	// console.log('num1', num1)
+	// console.log('num2', num2) 
 
 	// This line closes the connection to the command line interface.
 	reader.close()
 
 });
+
